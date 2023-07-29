48c83d2e977569c954a483e1bda07abca8833f17
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-07-29 10:49:51 +0600

    divice_token

---
+++ database/migrations/2023_07_29_044857_add_divice_token_to_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('otp', function (Blueprint $table) {
+                $table->string('divice_token', 30)->nullable();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['divice_token']);
+        });
+    }
+};
